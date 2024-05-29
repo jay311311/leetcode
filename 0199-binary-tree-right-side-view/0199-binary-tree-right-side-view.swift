e3f7010177a38111464eba2b0c0185f9e14c8ac5
jooeun kim <65765490+jay311311@users.noreply.github.com>
2024-05-29 10:41:16 -0400

    Time: 0 ms (100.00%), Space: 16.1 MB (22.76%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.swift
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func rightSideView(_ root: TreeNode?) -> [Int] {
+        var result:[Int] = []
+        recursive(root, &result, 0)
+        return result
+    }
+
+    func recursive(_ root: TreeNode?, _ list: inout [Int], _ depth: Int) {
+        if root == nil { return }
+
+        if list.count == depth {
+            list.append(root!.val)
+        }
+        recursive(root!.right, &list, depth + 1)
+        recursive(root!.left, &list, depth + 1)
+    }
+}
